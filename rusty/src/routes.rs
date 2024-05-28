d98437d4e7e676d1dad60bc9bc4fe1bcee263d93
Shughi <shugar94@hotmail.com>
2024-05-28 18:28:50 +0200

    add root endpoint

--- rusty/src/routes.rs
+++ rusty/src/routes.rs
@@ -1,22 +1,19 @@
 use axum::{Router, routing::get, Json};
-use serde_json::{Value};
+use serde_json::{Value, json};
 #[path = "nasa/nasa.rs"] mod nasa;
 
 pub async fn create_router() -> Router {
-    
     Router::new()
     .route("/", get(root))
     .route("/nasa/potd", get(nasa))
-    //.route("/foo/bar", get(bubu).post)
 }
 
-// which calls one of these handlers
-async fn root() { println!("ok"); }
+// root
+async fn root() -> Json<Value> {
+    Json(json!({ "HELLO": "WORLD" }))
+}
 
+// nasa
 async fn nasa() -> Json<Value> {
     nasa::potd().await
 }
-
-// async fn bubu() -> Json<Value> {
-//     Json(json!({ "data": 43 }))
-// }
