c6131103d870e544c602e98d140eb52d10023292
Shughi <shugar94@hotmail.com>
2024-05-28 18:18:43 +0200

    return json file

--- rusty/src/nasa/nasa.rs
+++ rusty/src/nasa/nasa.rs
@@ -1,6 +1,13 @@
-use axum::{Json};
+use axum::Json;
+use std::fs;
 use serde_json::{Value, json};
 
 pub async fn potd() -> Json<Value> {
-    Json(json!({ "data": 42 }))
+
+    let the_file = "ayylmao/potd.json";
+
+    let file = fs::File::open(the_file).expect("file should open read only");
+    let json: serde_json::Value = serde_json::from_reader(file).expect("file should be proper JSON");
+
+    Json(json!(json))
 }
