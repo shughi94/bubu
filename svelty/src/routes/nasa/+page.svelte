0ee5798eb5d1cf25669d5fa2e117519c14c41944
Shughi <shugar94@hotmail.com>
2024-05-29 10:50:57 +0200

    add image support for nasa potd

--- svelty/src/routes/nasa/+page.svelte
+++ svelty/src/routes/nasa/+page.svelte
@@ -1,29 +1,29 @@
 <script>
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
-	import Youtube from "svelte-youtube-embed";
+	import Youtube from 'svelte-youtube-embed';
+	import Image from '../../blocks/Image.svelte'
 
 	function goToPage() {
 		goto(`/`);
 	}
 
 	let data = null;
-	let video_id = null
-	let thumbnailUrl = null
+	let video_id = null;
+	let thumbnailUrl = null;
 	let error = null;
 	let loading = true;
 
 	onMount(async () => {
 		try {
 			const response = await fetch('http://localhost:3000/nasa/potd');
-			console.log(response)
+			console.log(response);
 			if (!response.ok) {
 				throw new Error('Failed to fetch data');
 			}
 			data = await response.json();
-			video_id = data.url.replace("https://www.youtube.com/embed/", "").replace("?rel=0","");
-			thumbnailUrl = "https://img.youtube.com/vi/${video_id}/maxresdefault.jpg"; //TODO?
-
+			video_id = data.url.replace('https://www.youtube.com/embed/', '').replace('?rel=0', '');
+			thumbnailUrl = 'https://img.youtube.com/vi/${video_id}/maxresdefault.jpg'; //TODO?
 		} catch (err) {
 			error = err.message;
 		} finally {
@@ -32,12 +32,54 @@
 	});
 </script>
 
+<h1>CIAO123</h1>
+<div class="container">
+	{#if loading}
+		<p class="loading">Loading...</p>
+	{:else if error}
+		<p class="error">Error: {error}</p>
+	{:else}
+		<div class="data-field">
+			<p>Date:</p>
+			<span>{data.date}</span>
+		</div>
+		<div class="data-field">
+			<p>Explanation:</p>
+			<span>{data.explanation}</span>
+		</div>
+		<div class="data-field">
+			<p>Title:</p>
+			<span>{data.title}</span>
+		</div>
+		{#if data.media_type == 'video'}
+			<div class="data-field">
+				<Youtube id={video_id}></Youtube>
+			</div>
+		{:else if data.media_type == 'image'}
+			<div class="data-field">
+				<Image class="image" src={data.url} alt="potd"/>
+			</div>
+		{:else}
+			<div class="data-field">
+				<p>No support found for: {data.media_type}</p>
+			</div>
+		{/if}
+	{/if}
+</div>
+
+<button on:click={goToPage}>BACK</button>
+
 <style>
 	.container {
 		max-width: 600px;
 		margin: 0 auto;
 		padding: 20px;
 		font-family: Arial, sans-serif;
+	}
+
+	.image {
+		width: var(--size);
+		height: auto;
 	}
 
 	.error {
@@ -60,30 +102,3 @@
 		margin-left: 10px;
 	}
 </style>
-
-<h1>CIAO123</h1>
-<div class="container">
-	{#if loading}
-		<p class="loading">Loading...</p>
-	{:else if error}
-		<p class="error">Error: {error}</p>
-	{:else}
-		<div class="data-field">
-			<p>Date:</p>
-			<span>{data.date}</span>
-		</div>
-		<div class="data-field">
-			<p>Explanation:</p>
-			<span>{data.explanation}</span>
-		</div>
-		<div class="data-field">
-			<p>Title:</p>
-			<span>{data.title}</span>
-		</div>
-		<div class="data-field">
-			<Youtube id={video_id}></Youtube>
-		</div>
-	{/if}
-</div>
-
-<button on:click={goToPage}>BACK</button>
