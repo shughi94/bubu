2559ca35fe95f0b5d5e3374c2125f5764577bfd3
Shughi <shugar94@hotmail.com>
2025-11-16 18:56:25 +0100

    change back button size

--- svelty/src/components/backButton.svelte
+++ svelty/src/components/backButton.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <button class="back-button" on:click={goToPage}>
-	<i class="fa fa-arrow-left fa-xl"></i>
+	<i class="fa fa-arrow-left"></i>
 </button>
 
 <style>
@@ -18,14 +18,11 @@
 	z-index: 100;
 	margin: 10px;
 	padding: 8px 15px;
-	background-color: rgba(44, 44, 44, 0.95);
+	background-color: rgba(11, 11, 11, 0.95);
 	color: white;
 	border: 2px solid white;
 	border-radius: 80%;
-	/* font-size: 100%; */
-	font-weight: 600;
 	cursor: pointer;
-	backdrop-filter: blur(10px);
 	-webkit-tap-highlight-color: transparent;
 	touch-action: manipulation;
 	min-height: 80px;
@@ -37,7 +34,6 @@
 	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
 	transition: all 0.2s ease;
 	align-self: flex-start;
-	width: fit-content;
 }
 
 .back-button:active {
@@ -46,7 +42,7 @@
 }
 
 .back-button i {
-	font-size: 0.9rem;
+	font-size: 3rem;
 }
 	
 </style>
